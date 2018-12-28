c6769dfe9305a08ac58e0767d532a66386870855
Denton24646 <Denton24646@gmail.com>
2018-12-28 18:04:25 -0500

    add interval parsing; add countdown descriptiontext

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,10 @@ use std::error::Error;
 use std::env;
 use std::time::{Duration};
 use std::thread::sleep;
+use std::string::String;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!(">>Waiting {} {}...", config.time, config.interval);
+    println!(">>Waiting {} {} {}", config.time, config.interval, config.countdown_description(config.countdown));
 
     let mut duration = Duration::from_secs(config.time);
     let increment = Duration::new(1, 0);
@@ -28,8 +29,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 pub struct Config {
     pub time: u64,
-    pub countdown: bool,
     pub interval: String,
+    pub countdown: bool,
 }
 
 impl Config {
@@ -37,21 +38,43 @@ impl Config {
         args.next();
 
         let time = match args.next() {
-            Some(arg) => arg.parse().unwrap(),
+            Some(arg) => arg.parse().unwrap(), 
             None => return Err("Didn't get a timer amount"),
         };
 
         let interval = match args.next() {
-            Some(arg) => arg.parse().unwrap(), //TODO parse into string and check if "m" or "s"
+            Some(arg) => {
+                let mut i: String = arg.parse().unwrap();
+                match i.as_ref() {
+                    "s" | "sec" | "seconds" => { i = String::from("seconds");},
+                    "m" | "min" | "minutes" => { i = String::from("minutes");},
+                    _ => return Err("Unsupported interval amount"),
+                }
+                i
+            },
             None => return Err("Didn't get an interval amount"),
         };
 
         let countdown = match args.next() {
-            Some(arg) => !arg.is_empty(), //TODO fix hack
+            Some(arg) => {
+                let mut c: bool = arg.parse().unwrap();
+                match c {
+                    true => {c = true;},
+                    false => {c = false;}
+                }
+                c
+            }
             None => return Err("Didn't get a countdown boolean"),
         };
 
         Ok(Config { time, interval, countdown })
+    }
+
+    pub fn countdown_description(&self, c: bool) -> String {
+        match c {
+            true => {let r = String::from("counting down..."); return r},
+            false => {let r = String::from("counting up..."); return r},
+        };
     }
 }
 
